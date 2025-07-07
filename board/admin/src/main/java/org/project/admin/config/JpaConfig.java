1c5e05a9bb2bbf316f6ef6c9f35e92820e8a6411
jitae5moon <jitae5moon@gmail.com>
2025-07-07 22:54:57 +0900

    feat: Add Jpa related config and entity
    - Should change AuditorAware value after spring security being added

---
+++ board/admin/src/main/java/org/project/admin/config/JpaConfig.java
@@ -0,0 +1,19 @@
+package org.project.admin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return () -> Optional.of("TEST_ADMIN");
+    }
+
+}
---
+++ board/admin/src/main/java/org/project/admin/domain/BaseEntity.java
@@ -0,0 +1,31 @@
+package org.project.admin.domain;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @CreatedBy
+    private String createdBy;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+    @LastModifiedBy
+    private String modifiedBy;
+
+}
