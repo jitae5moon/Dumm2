4b5d993df22ccecd1882c5d23b0e3b508123d697
jitae5moon <jitae5moon@gmail.com>
2025-07-08 21:57:48 +0900

    feat: Add repository method for logical deletion

--- board/admin/src/main/java/org/project/admin/repository/PostRepository.java
+++ board/admin/src/main/java/org/project/admin/repository/PostRepository.java
@@ -1,7 +1,12 @@
 package org.project.admin.repository;
 
 import org.project.admin.domain.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    Page<Post> findByIsDeletedFalse(Pageable pageable);
+
 }
--- board/admin/src/test/java/org/project/admin/repository/PostRepositoryTest.java
+++ board/admin/src/test/java/org/project/admin/repository/PostRepositoryTest.java
@@ -1,13 +1,20 @@
 package org.project.admin.repository;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.project.admin.config.JpaConfig;
 import org.project.admin.domain.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("test")
@@ -19,16 +26,38 @@ class PostRepositoryTest {
     PostRepository postRepository;
 
     @Test
-    void createPost() {
+    void givenPost_whenSavesPost_thenReturnsSavedPost() {
         // Given
-        Post post = Post.of("Test title", "Test content");
+        Post post = createPost();
+
+        // When
+        Post savedPost = postRepository.save(post);
+
+        // Then
+        assertThat(postRepository.findAll()).hasSize(1);
+        assertThat(postRepository.findById(savedPost.getId()).get().getCreatedBy()).isEqualTo("TEST_ADMIN");
+    }
+
+    @Test
+    void givenSavedPost_whenFindingPostWithIsDeletedFalse_thenReturnsPost() {
+        // Given
+        Post post = createPost();
+        Pageable pageable = createPageable();
 
         // When
         postRepository.save(post);
 
         // Then
-        assertThat(postRepository.findAll()).hasSize(1);
-        assertThat(postRepository.findById(1L).get().getCreatedBy()).isEqualTo("TEST_ADMIN");
+        Page<Post> result = postRepository.findByIsDeletedFalse(pageable);
+        assertThat(result.getTotalElements()).isEqualTo(1L);
+    }
+
+    private Post createPost() {
+        return Post.of("Test title", "Test content");
+    }
+
+    private Pageable createPageable() {
+        return PageRequest.of(0, 10, Sort.by("modifiedDate").descending());
     }
 
 }
