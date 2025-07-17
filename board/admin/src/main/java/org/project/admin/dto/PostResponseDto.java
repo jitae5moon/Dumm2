ef078f78356b11704a02e587d25f8df5265ed814
jitae5moon <jitae5moon@gmail.com>
2025-07-18 00:27:29 +0900

    feat: Add meta data fields in response dto

--- board/admin/src/main/java/org/project/admin/dto/PostResponseDto.java
+++ board/admin/src/main/java/org/project/admin/dto/PostResponseDto.java
@@ -2,20 +2,28 @@ package org.project.admin.dto;
 
 import org.project.admin.domain.Post;
 
+import java.time.LocalDateTime;
+
 public record PostResponseDto(
         Long id,
         String title,
         String content,
+        Boolean isNotice,
         Integer viewCount,
-        Integer likeCount
+        Integer likeCount,
+        String createdBy,
+        LocalDateTime createdDate
 ) {
 
     public static PostResponseDto from(Post post) {
         return new PostResponseDto(post.getId(),
                 post.getTitle(),
                 post.getContent(),
+                post.getIsNotice(),
                 post.getViewCount(),
-                post.getLikeCount());
+                post.getLikeCount(),
+                post.getCreatedBy(),
+                post.getCreatedDate());
     }
 
 }
